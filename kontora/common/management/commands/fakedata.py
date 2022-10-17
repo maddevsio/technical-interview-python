824eeb739d852313a8bd5a29ad6f220fa003760a
Aleksandr Edakin <alexeagro98@gmail.com>
2022-10-17 13:28:03 +0600

    Update fakedata generator

--- kontora/common/management/commands/fakedata.py
+++ kontora/common/management/commands/fakedata.py
@@ -12,32 +12,35 @@ class Command(BaseCommand):
     help = "Create fake data for the development environment."
 
     number_of_customers = 100
-    number_of_debts_by_customer = 1000
+    number_of_debts_per_customer = 6
 
-    number_of_contracts = 100
-    number_of_debts_by_contract = 100
+    number_of_contracts_per_customer = 100
+    number_of_debts_per_contract = 3
 
     def handle(self, *args, **options):
-        debts_count = self.number_of_contracts + self.number_of_debts_by_contract
-        amount_list = (randrange(1000, 100000, debts_count) / 1000 for _ in range(debts_count))
+        def amount_generator():
+            while True:
+                yield randrange(1, 500)
+
+        amount = amount_generator()
 
         Customer.objects.bulk_create((Customer(fullname=f"John Doe {i}") for i in range(self.number_of_customers)))
         customers = Customer.objects.all()
         infinity_customers = cycle(customers)
 
         Debt.objects.bulk_create(
-            (Debt(customer=next(infinity_customers), amount=next(amount_list)) for _ in range(self.number_of_contracts))
+            (Debt(customer=next(infinity_customers), amount=next(amount)) for _ in range(self.number_of_customers * self.number_of_debts_per_customer))
         )
 
         Contract.objects.bulk_create(
-            (Contract(customer=next(infinity_customers)) for _ in range(self.number_of_debts_by_customer))
+            (Contract(customer=next(infinity_customers)) for _ in range(self.number_of_customers * self.number_of_contracts_per_customer))
         )
         contracts = Contract.objects.all()
         infinity_contracts = cycle(contracts)
 
         Debt.objects.bulk_create(
             (
-                Debt(contract=next(infinity_contracts), amount=next(amount_list))
-                for _ in range(self.number_of_debts_by_contract)
+                Debt(contract=next(infinity_contracts), amount=next(amount))
+                for _ in range(self.number_of_customers * self.number_of_contracts_per_customer * self.number_of_debts_per_contract)
             )
         )
