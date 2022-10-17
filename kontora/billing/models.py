e872c482099908803646aaf2804bacf8d5b1d4b0
Aleksandr Edakin <alexeagro98@gmail.com>
2022-10-17 13:31:47 +0600

    add related_name to FKs

--- kontora/billing/models.py
+++ kontora/billing/models.py
@@ -3,8 +3,20 @@ from django.db import models
 
 class Debt(models.Model):
     amount = models.DecimalField(max_digits=6, decimal_places=2)
-    customer = models.ForeignKey("customers.Customer", on_delete=models.CASCADE, null=True, blank=True)
-    contract = models.ForeignKey("contracts.Contract", on_delete=models.CASCADE, null=True, blank=True)
+    customer = models.ForeignKey(
+        "customers.Customer",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="debts",
+    )
+    contract = models.ForeignKey(
+        "contracts.Contract",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="debts"
+    )
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- kontora/contracts/models.py
+++ kontora/contracts/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Contract(models.Model):
     identifier = models.UUIDField(default=uuid.uuid4)
-    customer = models.ForeignKey("customers.Customer", on_delete=models.CASCADE)
+    customer = models.ForeignKey("customers.Customer", on_delete=models.CASCADE, related_name="contracts")
 
     def __str__(self):
         return f"{self.identifier}"
